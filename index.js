33ef64df288873a98495a29f345ac305b847cb25
ropeks <ropeksmilioner@gmail.com>
2019-07-10 15:20:01 +0200

    set up server, create logger middleware and get endpoint

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // code away!
+const server = require('./server');
+const port = 3000;
+
+server.listen(port, ()=> {
+    console.log(`server running on port ${port}`);
+});
--- server.js
+++ server.js
@@ -1,6 +1,10 @@
-const express = 'express';
+const express = require('express');
+const User = require("./users/userDb");
 
 const server = express();
+
+server.use(express.json());
+server.use(logger);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
@@ -9,7 +13,28 @@ server.get('/', (req, res) => {
 //custom middleware
 
 function logger(req, res, next) {
-
+  const message = {
+    method: req.method,
+    url: req.url,
+    time: new Date()
+  };
+  console.log(message);
+  next();
 };
+
+// API
+
+server.get('/api/users', (req, res) => {
+  User
+    .get()
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({message: 'cannot get users'})
+    })
+})
 
 module.exports = server;
