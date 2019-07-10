28c8e2d8906fb76c7167797addcfe2d1cdca202f
ropeks <ropeksmilioner@gmail.com>
2019-07-10 16:13:22 +0200

    create validateUser middleware and post endpoint

--- server.js
+++ server.js
@@ -34,6 +34,24 @@ async function validateUserId(req, res, next) {
   }
 }
 
+async function validateUser(req, res, next) {
+  const { body } = req;
+  console.log(body);
+  if (body.length>0) {
+    if (body.name) {
+      next();
+    } else {
+      res
+        .status(400)
+        .json({message: "missing required name field"});
+    }
+  } else {
+    res
+      .status(400)
+      .json({message: "missing user data"});
+  }
+}
+
 // API
 
 server.get('/api/users', (req, res) => {
@@ -60,6 +78,17 @@ server.get('/api/users/:id', validateUserId, (req, res) => {
         .status(500)
         .json({message: 'cannot retrieve that user'})
     })
+})
+
+server.post('/api/users', validateUser, async (req, res) => {
+  try {
+    const user = await User.insert(req.body);
+    res.status(200).json(user);
+  } catch {
+    res
+      .status(500)
+      .json({message: 'cannot add new user'});
+  }
 })
 
 module.exports = server;
